b68936974905d1fa80ec1e73c36117477bc87ab3
Shahana <s223235509@deakin.edu.au>
2023-08-21 19:20:15 +1000

    initial commit

---
+++ routers/route.js
@@ -0,0 +1,16 @@
+let express = require('express');
+let router = express.Router();
+let controller = require('../controllers/controller');
+
+router.post('/', function (req, res) {
+
+    controller.postCat(req, res);
+});
+
+router.get('/', (req, res) => {
+
+    controller.getAllCats(req, res);
+
+});
+
+module.exports = router;
